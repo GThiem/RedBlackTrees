08253e5282936a2fe2d7407a60ec9b856603b0c9
Gabriel <gabrielthiem@gmail.com>
2020-05-31 19:10:45 +0200

    sentinel???

--- RedBlackTrees/src/lab/RedBlackTree.java
+++ RedBlackTrees/src/lab/RedBlackTree.java
@@ -8,6 +8,7 @@ package lab;
 
 
 import frame.TreeNode;
+import frame.TreeNode.NodeColor;
 
 /**
  * An implementation of a Black-Red-Tree
@@ -17,9 +18,19 @@ public class RedBlackTree {
 	private TreeNode _root;
 	private TreeNode _nil;
 	
+	//????????????????
+	private TreeNode sent;
+	
 	public RedBlackTree() {
 		_nil = new TreeNode();
 		_root = _nil;
+		
+		//????????????????
+		sent = new TreeNode();
+		sent.p = sent;
+		sent.left = sent;
+		sent.right = sent;
+		_root.p = sent;
 	}
 	
 	public TreeNode root() {
@@ -35,7 +46,23 @@ public class RedBlackTree {
 	 */
 	public TreeNode search(int key) {
         // TODO: Implement
-		throw new RuntimeException("Not implemented yet!");
+		return searchRec(_root, key);
+		
+	}
+	
+	//recursion for search
+	public TreeNode searchRec(TreeNode x, int key) {
+		
+		if(x == _nil) 
+			return _nil;
+		
+		if(x.key == key)
+			return x;
+		
+		if(x.key > key)
+			return searchRec(x.left, key);
+		else
+			return searchRec(x.right, key);
 	}
 	
 	/**
@@ -50,7 +77,14 @@ public class RedBlackTree {
 	 */
 	public TreeNode minimumSubtree(TreeNode x) {
 		// TODO: Implement
-		throw new RuntimeException("Not implemented yet!");
+		if(x == _nil)
+			return _nil;
+		
+		while(x.left != _nil)
+			x = x.left;
+		
+		return x;
+		
 	}
 	
 	/**
@@ -65,7 +99,13 @@ public class RedBlackTree {
 	 */
 	public TreeNode maximumSubtree(TreeNode x) {
 		// TODO: Implement
-		throw new RuntimeException("Not implemented yet!");
+		if(x == _nil)
+			return _nil;
+		
+		while(x.right != _nil)
+			x = x.right;
+		
+		return x;
 	}
 
 	/**
@@ -73,7 +113,33 @@ public class RedBlackTree {
 	 */
 	public void insert(TreeNode newNode) {
 		// TODO: Implement
-		throw new RuntimeException("Not implemented yet!");
+		TreeNode x = _root;
+		TreeNode px = sent;
+		newNode.right = newNode.left = _nil;
+		
+		while(x != _nil) {
+			px=x;
+			if(x.key > newNode.key) {
+				x = x.left;
+			} else {
+				x = x.right;
+			}
+		}
+		newNode.p = px;
+		
+		if(px == sent) {
+			_root = newNode;
+		} else {
+			if(px.key > newNode.key) {
+				px.left = newNode;
+			} else {
+				px.right = newNode;
+			}
+		}
+		
+		newNode.color = NodeColor.RED;
+		fixColorAfterInsertion(newNode);
+		
 	}
 	
 	/**
@@ -81,7 +147,103 @@ public class RedBlackTree {
 	 */
 	public void delete(TreeNode toDelete) {
 		// TODO: Implement
-		throw new RuntimeException("Not implemented yet!");
 	}
-
+	
+	//left rotation
+	public void rotateLeft(TreeNode x) {
+		
+		TreeNode y = x.right;
+		x.right = y.left;
+		
+		if(y.left != _nil) {
+			y.left.p = x;
+		}
+		
+		y.p = x.p;
+		
+		if(x.p == sent) {
+			_root = y;
+		} else {
+			if(x == x.p.left) {
+				x.p.left = y;
+			} else {
+				x.p.right = y;
+			}
+		}
+		
+		y.left = x;
+		x.p = y;
+	}
+	
+	//right rotation
+	public void rotateRight(TreeNode x) {
+		
+		TreeNode y = x.left;
+		x.left = y.right;
+		
+		if(y.right != _nil) {
+			y.right.p = x;
+		}
+		
+		y.p = x.p;
+		
+		if(x.p == sent) {
+			_root = y;
+		} else {
+			if(x == x.p.right) {
+				x.p.right = y;
+			} else {
+				x.p.left = y;
+			}
+		}
+		
+		y.right = x;
+		x.p = y;
+	}
+	
+	public void fixColorAfterInsertion(TreeNode z) {
+		
+		while(z.p.color == NodeColor.RED) {
+			if(z.p == z.p.p.left) {
+				
+				TreeNode y = z.p.p.right;
+				if(y != _nil && y.color == NodeColor.RED) {
+					z.p.color = NodeColor.BLACK;
+					y.color = NodeColor.BLACK;
+					z.p.p.color = NodeColor.RED;
+					z = z.p.p;
+				} else {
+					if(z == z.p.right) {
+						z = z.p;
+						rotateLeft(z);
+					}
+					z.p.color = NodeColor.BLACK;
+					z.p.p.color = NodeColor.RED;
+					rotateRight(z.p.p);
+				}
+				
+			} else {
+				
+				TreeNode y = z.p.p.left;
+				if(y != _nil && y.color == NodeColor.RED) {
+					z.p.color = NodeColor.BLACK;
+					y.color = NodeColor.BLACK;
+					z.p.p.color = NodeColor.RED;
+					z = z.p.p;
+				} else {
+					if(z == z.p.left) {
+						z = z.p;
+						rotateRight(z);
+					}
+					z.p.color = NodeColor.BLACK;
+					z.p.p.color = NodeColor.RED;
+					rotateLeft(z.p.p);
+				}
+			}
+		}
+		
+		_root.color = NodeColor.BLACK;
+		
+	}
+	
 }
